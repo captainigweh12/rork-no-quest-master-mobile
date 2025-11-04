a37969a48fbf164119e3543122acfa15e0539ef2
captainigweh12 <captainigweh12@gmail.com>
2025-11-04 18:27:10 -0500

    Create transformer.ts

---
+++ lib/transformer.ts
@@ -0,0 +1,10 @@
+export const transformer = {
+  input: {
+    serialize: (data: unknown) => data,
+    deserialize: (data: unknown) => data,
+  },
+  output: {
+    serialize: (data: unknown) => data,
+    deserialize: (data: unknown) => data,
+  },
+};
